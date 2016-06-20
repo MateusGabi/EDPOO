b84e1716a46610fce7d0cc2df354c48e1e1fbfb5
Mateus Gabi <mateusgabimoreira@hotmail.com>
2016-06-20 16:08:23 -0400

    tarefa 22.06

--- src/Models/ArvoreAVL.java
+++ src/Models/ArvoreAVL.java
@@ -1,43 +1,43 @@
 package Models;
-
 import java.util.Deque;
 import java.util.LinkedList;
 
 /**
- * Árvore AVL.
+ * Ã�rvore AVL.
  * 
- * Um árvore AVL é uma árvore binária de busca que mantém o balanceamento entre
- * a altura das sub-árvores esquerda e direita em todos os nós da árvore. Desta
- * forma, as operações de busca, inserção e remoção tem custo assintótico igual
- * a O(log(n)).
+ * Um Ã¡rvore AVL Ã© uma Ã¡rvore binÃ¡ria de busca que mantÃ©m o balanceamento entre
+ * a altura das sub-Ã¡rvores esquerda e direita em todos os nÃ³s da Ã¡rvore. Desta
+ * forma, as operaÃ§Ãµes de busca, inserÃ§Ã£o e remoÃ§Ã£o tem custo assintÃ³tico igual
+ * a O(log👎).
  * 
  * @author eraldo
  * 
  */
 public class ArvoreAVL<T extends Comparable<T>> extends ArvoreBinariaBusca<T> {
 
+
 	/**
 	 * Retorna a raiz convertida para <code>NoAVL</code>.
 	 * 
 	 * <p>
 	 * Como esta classe extende a classe <code>ArvoreBinariaBusca</code>, cujo
-	 * atributo (herdado aqui) é do tipo <code>No</code>, precisamos
-	 * frequentemente converter a referência para o nó raiz para o tipo correto
-	 * neste caso que é <code>NoAVL</code>. Para evitar esta conversão em vários
-	 * pontos do código, criamos este método.
+	 * atributo (herdado aqui) Ã© do tipo <code>No</code>, precisamos
+	 * frequentemente converter a referÃªncia para o nÃ³ raiz para o tipo correto
+	 * neste caso que Ã© <code>NoAVL</code>. Para evitar esta conversÃ£o em vÃ¡rios
+	 * pontos do cÃ³digo, criamos este mÃ©todo.
 	 * </p>
 	 * 
-	 * @return referência para o nó raiz do tipo <code>NoAVL<T></code>.
+	 * @return referÃªncia para o nÃ³ raiz do tipo <code>NoAVL<T></code>.
 	 */
 	public NoAVL<T> getRaiz() {
 		return (NoAVL<T>) raiz;
 	}
 
 	/**
-	 * Insere o valor <code>info</code> na árvore.
+	 * Insere o valor <code>info</code> na Ã¡rvore.
 	 * 
 	 * @param info
-	 *            valor a ser inserido no árvore.
+	 *            valor a ser inserido no Ã¡rvore.
 	 */
 	public void insere(T info) {
 		if (raiz == null) {
@@ -49,31 +49,28 @@ public class ArvoreAVL<T extends Comparable<T>> extends ArvoreBinariaBusca<T> {
 	}
 
 	/**
-	 * Insere (recursivamente) o valor <code>val</code> na sub-árvore cuja raiz
-	 * é <code>n</code>.
+	 * Insere (recursivamente) o valor <code>val</code> na sub-Ã¡rvore cuja raiz
+	 * Ã© <code>n</code>.
 	 * 
 	 * @param n
-	 *            nó da sub-árvore onde o novo valor deve ser inserido.
+	 *            nÃ³ da sub-Ã¡rvore onde o novo valor deve ser inserido.
 	 * @param pai
-	 *            nó pai de <code>n</code>.
+	 *            nÃ³ pai de <code>n</code>.
 	 * @param filhoEsq
-	 *            indica se o <code>n</code> é filho esquerdo de
+	 *            indica se o <code>n</code> Ã© filho esquerdo de
 	 *            <code>pai</code>.
 	 * @param val
 	 *            valor a ser inserido.
-	 * @return <code>true</code> se a altura da sub-árvore cuja raiz é
-	 *         <code>n</code> aumentou com a inserção; <code>false</code> caso
-	 *         contrário.
+	 * @return <code>true</code> se a altura da sub-Ã¡rvore cuja raiz Ã©
+	 *         <code>n</code> aumentou com a inserÃ§Ã£o; <code>false</code> caso
+	 *         contrÃ¡rio.
 	 */
 	private boolean insere(NoAVL<T> n, NoAVL<T> pai, boolean filhoEsq, T val) {
 		if (val.compareTo(n.info) < 0) {
 			if (n.esq == null) {
 				n.esq = new NoAVL<T>(val);
-				
-				++n.fb;
-				
+				n.fb +=1;
 				return n.fb != 0;
-//				return false;
 			}
 			if (insere(n.getEsq(), n, true, val)) {
 				n.fb++;
@@ -86,135 +83,141 @@ public class ArvoreAVL<T extends Comparable<T>> extends ArvoreBinariaBusca<T> {
 				return n.fb != 0;
 			}
 			return false;
-		} else {
-			if (n.dir == null) {
+		} else {			
+			if(n.dir == null){
 				n.dir = new NoAVL<T>(val);
-				--n.fb;
-				System.out.println("oww" + val);
-//				return n.fb != 0;
-				return true;
+				n.fb -=1;
+				return n.fb != 0;
 			}
-			if (insere(n.getDir(), n, false, val)) {
+			if(insere(n.getDir(), n, false, val)){
 				n.fb--;
-				if (n.fb < -1) {
-					if (n.getDir().fb > 0)
+				if(n.fb < -1){
+					if(n.getDir().fb > 0)
 						rotDir(n.getDir(), n, false);
 					rotEsq(n, pai, filhoEsq);
-					return true;
+					return false;
 				}
 				return n.fb != 0;
 			}
-			return true;
+			return false;
 		}
 	}
 
 	/**
-	 * Rotaciona à direita a sub-árvore com raiz em <code>n</code>.
+	 * Rotaciona Ã  direita a sub-Ã¡rvore com raiz em <code>n</code>.
 	 * 
 	 * @param x
-	 *            raiz da sub-árvore a ser rotacionada.
+	 *            raiz da sub-Ã¡rvore a ser rotacionada.
 	 * @param pai
-	 *            pai do nó <code>n</code>.
+	 *            pai do nÃ³ <code>n</code>.
 	 * @param filhoEsq
-	 *            indica se <code>n</code> é filho esquerdo de <code>pai</code>.
+	 *            indica se <code>n</code> Ã© filho esquerdo de <code>pai</code>.
 	 */
 	private void rotDir(NoAVL<T> x, NoAVL<T> pai, boolean filhoEsq) {
 		NoAVL<T> z = x.getEsq();
+		z = x.getEsq();
 		x.esq = z.getDir();
 		z.dir = x;
-		x.fb--;
-		if (z.fb > 0)
+		x.fb -= 1;
+		if(z.fb > 0)
 			x.fb -= z.fb;
-		z.fb -= 1;
-		if (x.fb < 0)
+		z.fb -=1;
+		if(x.fb < 0)
 			z.fb += x.fb;
-		if (pai == null)
+		if(pai == null)
 			raiz = z;
-		else if (filhoEsq) {
+		else if(filhoEsq){
 			pai.esq = z;
-		} else {
+		}
+		else{
 			pai.dir = z;
 		}
 	}
 
 	/**
-	 * Rotaciona à esquerda a sub-árvore com raiz em <code>n</code>.
+	 * Rotaciona Ã  esquerda a sub-Ã¡rvore com raiz em <code>n</code>.
 	 * 
 	 * @param x
-	 *            raiz da sub-árvore a ser rotacionada.
+	 *            raiz da sub-Ã¡rvore a ser rotacionada.
 	 * @param pai
-	 *            pai do nó <code>n</code>.
+	 *            pai do nÃ³ <code>n</code>.
 	 * @param filhoEsq
-	 *            indica se <code>n</code> é filho esquerdo de <code>pai</code>.
+	 *            indica se <code>n</code> Ã© filho esquerdo de <code>pai</code>.
 	 */
 	private void rotEsq(NoAVL<T> x, NoAVL<T> pai, boolean filhoEsq) {
 		NoAVL<T> z = x.getDir();
+		z = x.getDir();
 		x.dir = z.getEsq();
 		z.esq = x;
-		x.fb++;
-		if (z.fb < 0)
+		x.fb += 1;
+		if(z.fb < 0)
 			x.fb -= z.fb;
-		if (x.fb > 0)
+		z.fb +=1;
+		if(x.fb > 0)
 			z.fb += x.fb;
-		if (pai == null)
+		if(pai == null)
 			raiz = z;
-		else if (filhoEsq) {
+		else if(filhoEsq){
 			pai.esq = z;
-		} else {
+		}
+		else{
 			pai.dir = z;
 		}
-
+		
 	}
 
 	/**
-	 * Verifica os fatores de balanceamento de todos os nós da árvore.
+	 * Verifica os fatores de balanceamento de todos os nÃ³s da Ã¡rvore.
 	 * 
 	 * @return <code>true</code> caso todos os FBs estejam corretos;
-	 *         <code>false</code> caso contrário.
+	 *         <code>false</code> caso contrÃ¡rio.
 	 */
 	public boolean verificaFBs() {
 		return verificaFBs(getRaiz()) != Integer.MIN_VALUE;
 	}
 
 	/**
-	 * Verifica se os fatores de balanceamento dos nós da sub-árvore cuja raiz é
-	 * <code>n</code> estão corretos.
+	 * Verifica se os fatores de balanceamento dos nÃ³s da sub-Ã¡rvore cuja raiz Ã©
+	 * <code>n</code> estÃ£o corretos.
 	 * 
 	 * @param n
-	 *            raiz da sub-árvore a ser verificada.
-	 * @return altura da sub-árvore cuja raiz é <code>n</code>.
+	 *            raiz da sub-Ã¡rvore a ser verificada.
+	 * @return altura da sub-Ã¡rvore cuja raiz Ã© <code>n</code>.
 	 */
 	private int verificaFBs(NoAVL<T> n) {
 		if (n == null)
 			return 0;
 
 		/*
-		 * Calcula altura da sub-árvore esquerda enquanto verifica os fatores de
-		 * balanceamentos dos seus nós.
+		 * Calcula altura da sub-Ã¡rvore esquerda enquanto verifica os fatores de
+		 * balanceamentos dos seus nÃ³s.
 		 */
 		int hEsq = verificaFBs(n.getEsq());
 		if (hEsq == Integer.MIN_VALUE)
 			return Integer.MIN_VALUE;
 
 		/*
-		 * Calcula altura da sub-árvore direita enquanto verifica os fatores de
-		 * balanceamentos dos seus nós.
+		 * Calcula altura da sub-Ã¡rvore direita enquanto verifica os fatores de
+		 * balanceamentos dos seus nÃ³s.
 		 */
 		int hDir = verificaFBs(n.getDir());
 		if (hDir == Integer.MIN_VALUE)
 			return Integer.MIN_VALUE;
 
 		if (hEsq - hDir != n.fb) {
-			System.err.printf("Erro no nó %s! FB=%d mas deveria ser %d\n", n, n.fb, hEsq - hDir);
+			System.err.printf("Erro no nÃ³ %s! FB=%d mas deveria ser %d\n", n,
+					n.fb, hEsq - hDir);
 			return Integer.MIN_VALUE;
 		}
 
 		if (n.fb < -1 || n.fb > 1) {
-			System.err.printf("Erro no nó %s! FB=%d (fora do intervalo válido)!\n", n, n.fb);
+			System.err.printf(
+					"Erro no nÃ³ %s! FB=%d (fora do intervalo vÃ¡lido)!\n", n,
+					n.fb);
 			return Integer.MIN_VALUE;
 		}
 
-		// Retorna a altura da sub-árvore cuja raiz é o nó n.
+		// Retorna a altura da sub-Ã¡rvore cuja raiz Ã© o nÃ³ n.
 		return 1 + Math.max(hEsq, hDir);
 	}
 }
--- src/Test/TesteArvoreAVL.java
+++ src/Test/TesteArvoreAVL.java
@@ -1,32 +1,149 @@
 package Test;
 
+import java.util.Calendar;
 import java.util.Random;
 
 import Models.ArvoreAVL;
+import Models.ArvoreBinariaBusca;
 
 public class TesteArvoreAVL {
+
+	private static double now() {
+		return Calendar.getInstance().getTimeInMillis() / 1000d;
+	}
 
 	/**
 	 * Quantidade de nÃºmeros a serem inseridos e buscados.
 	 */
 	private static final int LEN = 50;
 
+	private static void Test(ArvoreBinariaBusca tree, int n) {
+		
+		
+		
+	}
+
 	public static void main(String[] args) {
 		// Cria nova Ã¡rvore AVL.
-		ArvoreAVL<Integer> avl = new ArvoreAVL<Integer>();
 
 		// Insere nÃºmeros aleatoriamente.
-		Random r = new Random();
-		for (int i = 0; i < LEN; ++i) {
-			avl.insere(r.nextInt(10 * LEN));
+		// Random r = new Random();
+		// for (int i = 0; i < LEN; ++i) {
+		// avl.insere(r.nextInt(10 * LEN));
 
-			// Verifica fatores de balanceamento de toda a Ã¡rvore.
-			if (!avl.verificaFBs())
-				break;
-		}
+		// // Verifica fatores de balanceamento de toda a Ã¡rvore.
+		// if (!avl.verificaFBs())
+		// break;
+		// }
 
 		// Exibe Ã¡rvore em formato deitada.
-		avl.exibeDeitada();
+		/* avl.exibeDeitada(); */
+
+		int k = 4;
+		
+		// valor inicial
+		int n = 1000;
+
+		while (k-- > 0) {
+			
+			System.out.println("DOING..");
+			System.out.println("-------------------------------------------------------------");
+			
+			ArvoreAVL<Integer> avl = new ArvoreAVL<Integer>();
+			ArvoreBinariaBusca<Integer> abb = new ArvoreBinariaBusca();
+
+			double start = now();
+			
+			// inserir seq na ABB
+//			System.out.println();
+			for (int i = 0; i < n; i++) {
+				abb.insere(i);
+			}
+			System.out.println("Tempo de inserção sequencial na ABB de "+n +" valores: " + (now() - start));
+			
+			
+			
+			start = now();
+			
+			// busca seq na ABB
+			int temp = n;
+			while(temp > 0) {
+				abb.busca(temp);
+				temp--;
+			}
+			System.out.println("Tempo de busca sequencial na ABB de "+n +" valores: " + (now() - start));
+
+			
+			
+			start = now();
+			
+			// inserir seq na AVL			
+			for (int i = 0; i < n; i++) {
+				avl.insere(i);
+			}
+			System.out.println("Tempo de inserção sequencial na AVL de "+n +" valores: " + (now() - start));
+			
+			
+			start = now();
+			
+			// busca seq na ABB
+			temp = n;
+			while(temp > 0) {
+				abb.busca(temp);
+				temp--;
+			}
+			System.out.println("Tempo de busca sequencial na AVL de "+n +" valores: " + (now() - start));
+
+			// Aleatórios
+			Random r = new Random();
+			
+			start = now();
+			
+			// inserir seq na ABB
+//			System.out.println();
+			for (int i = 0; i < n; i++) {
+				abb.insere(r.nextInt(n));
+			}
+			System.out.println("Tempo de inserção aleatória na ABB de "+n +" valores: " + (now() - start));
+			
+			
+			
+			start = now();
+			
+			// busca seq na ABB
+			temp = 0;
+			while(temp > 0) {
+				abb.busca(r.nextInt(n));
+				temp--;
+			}
+			System.out.println("Tempo de busca aleatória na ABB de "+n +" valores: " + (now() - start));
+
+			
+			
+			start = now();
+			
+			// inserir seq na AVL			
+			for (int i = 0; i < n; i++) {
+				avl.insere(r.nextInt(n));
+			}
+			System.out.println("Tempo de inserção aleatória na AVL de "+n +" valores: " + (now() - start));
+			
+			
+			start = now();
+			
+			// busca seq na ABB
+			temp = n;
+			while(temp > 0) {
+				abb.busca(r.nextInt(n));
+				temp--;
+			}
+			System.out.println("Tempo de busca aleatória na AVL de "+n +" valores: " + (now() - start));
+
+			
+
+			n *= 10;
+			System.out.println("-------------------------------------------------------------");
+		}
 	}
 
 }
